6c4ffb2a40937e6898e884afef5ddf9b31d5f21a
Natan Borba <78612166+natanjborba@users.noreply.github.com>
2024-05-23 23:48:43 -0300

    Update install_java.sh

--- install_java.sh
+++ install_java.sh
@@ -8,12 +8,17 @@ sudo apt install -y openjdk-17-jdk
 
 # Altera a porta SSH para 51352 e ajusta as configurações de segurança
 sudo sed -i 's/^#Port 22/Port 51352/' /etc/ssh/sshd_config
+sudo sed -i 's/^Port 22/Port 51352/' /etc/ssh/sshd_config
 sudo sed -i 's/^#PermitRootLogin prohibit-password/PermitRootLogin yes/' /etc/ssh/sshd_config
+sudo sed -i 's/^PermitRootLogin prohibit-password/PermitRootLogin yes/' /etc/ssh/sshd_config
 sudo sed -i 's/^#PasswordAuthentication yes/PasswordAuthentication yes/' /etc/ssh/sshd_config
 sudo sed -i 's/^PasswordAuthentication no/PasswordAuthentication yes/' /etc/ssh/sshd_config
 
 # Reinicia o serviço SSH para aplicar as mudanças
 sudo systemctl restart sshd
+
+# Verifica a instalação do OpenJDK 17
+java -version
 
 # Exibe uma mensagem de conclusão
 echo "Configurações aplicadas com sucesso."
